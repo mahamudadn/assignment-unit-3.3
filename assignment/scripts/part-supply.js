dbf4cdb62c4450f7566999423e6ddb146587b9da
Mahamud Aden <mahamudadn@gmail.com>
2023-04-16 14:15:10 -0500

    Modified Q7 of Stretch

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -59,9 +59,15 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
-for (items of supplyChanges) {
-    console.log('For of loop', supplyChanges);
 
+for ( value of supplyChanges) {
+  if (value > 0) {
+    console.log(`Added ${value} parts.`);
+  } else if (value === 0) {
+    console.log('No Change.');
+  } else {
+    console.log(`Removed ${Math.abs(value)} parts.`);
+  }
 }
 
 // 8. Write a loop to determine the total number of parts available by
